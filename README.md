fbbaab9826d5872f53a232811963b47cfd6144f6
Sastha K L <sastha@solutionmatrix.com.sg>
2013-10-31 14:46:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# [AngularJS TransitJS Directive]
+
+Built on top of [http://html5boilerplate.com](http://html5boilerplate.com)
+Based on [http://ricostacruz.com/jquery.transit/#top](http://ricostacruz.com/jquery.transit/#top)
+
+## Features
+
+* Twitter Bootstrap v 3 
+* AngularJS 1.2.0 rc3
+* jQuery 2.0.3
+* HTML5 Boiler plate 4.2.0
+* AngularUI Bootstrap 0.6.0
+
+
+## Documentation
+
+In Progress
+
---
+++ js/app.js
@@ -0,0 +1,66 @@
+var smApp = angular.module('smApp',['ui.bootstrap','ui.router']);
+
+smApp.config(function($stateProvider, $urlRouterProvider) {
+
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/home.html",
+      controller: 'appController',
+    })
+    .state('404', {
+      url: "/404",
+      templateUrl: "404.html",
+      controller: 'app404Controller',
+    })
+    // For any unmatched url, redirect to /state1
+     $urlRouterProvider.otherwise("/404");
+
+});
+smApp.directive('transitCall', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          $(element).bind('mouseover', function (e) {
+            $(element).transition(scope.$eval(attrs.transitCall));
+          });
+        }
+    };
+});
+smApp.directive('transitRotate', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+          $(element).bind('mouseout', function (e) {
+            $(element).transition(scope.$eval(attrs.transitRotate));
+          });
+        }
+    };
+});
+//controller
+smApp.controller('appController', function($scope){
+
+	$scope.alerts = [];
+	$scope.searchText;
+	
+  	//Initializer
+	init();
+	function init(){
+		
+		
+	};
+	
+});
+smApp.controller('app404Controller', function($scope){
+
+});
+angular.module('smApp').run(function($http, $rootScope, $location) {
+
+//Active menu
+ $rootScope.isActive = function (viewLocation) {
+        console.log($location.path());
+        return viewLocation === $location.path();
+ };
+
+});
